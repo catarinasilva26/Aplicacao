53deabcf9ebbb982337b35af2f89fec0bddbe756
Catarina Silva <catarinasilva2000@gmail.com>
2021-03-29 16:19:33 +0100

    Criação da atividade do Login

---
+++ app/src/main/java/intro/android/aplicacao/Login.kt
@@ -0,0 +1,11 @@
+package intro.android.aplicacao
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Login : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
