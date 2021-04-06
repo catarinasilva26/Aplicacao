83abf65f65e2bed86a1d62a061852dceece4243d
Catarina Silva <catarinasilva2000@gmail.com>
2021-04-06 17:52:41 +0100

    Layout concluído

---
+++ app/src/main/java/intro/android/aplicacao/Menu.kt
@@ -0,0 +1,11 @@
+package intro.android.aplicacao
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Menu : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_menu)
+    }
+}
